7cf2bfc3eafbac567da1f0d2f1cc3db8fd504955
남은솔 <106670648+nameunsol@users.noreply.github.com>
2024-12-10 11:29:38 +0900

    🎉 시작

---
+++ app/src/main/java/kr/ac/kumoh/s20220382/s24w1403retrofit/MainActivity.kt
@@ -0,0 +1,47 @@
+package kr.ac.kumoh.s20220382.s24w1403retrofit
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import kr.ac.kumoh.s20220382.s24w1403retrofit.ui.theme.S24W1403RetrofitTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            S24W1403RetrofitTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    S24W1403RetrofitTheme {
+        Greeting("Android")
+    }
+}
